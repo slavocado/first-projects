9c319398c50480fd25f2fc3c97783959e281a355
Viacheslav <little.cado1@gmail.com>
2020-07-30 23:26:33 +0300

    Add project skeleton

---
+++ tenniscore/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function (){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
