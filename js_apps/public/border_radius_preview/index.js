79fdc438ed3023e57c05a83e11b3f42bbf2896c8
Viacheslav <little.cado1@gmail.com>
2020-09-03 18:28:13 +0300

    Add border radius heading and display current border property values

--- js_apps/public/border_radius_preview/index.js
+++ js_apps/public/border_radius_preview/index.js
@@ -3,6 +3,7 @@ const rightInput = document.getElementById('right-input');
 const bottomInput = document.getElementById('bottom-input');
 const leftInput = document.getElementById('left-input');
 const borderChanger = document.querySelector('.border-changer')
+const propertyShower = document.getElementById('property-shower')
 
 let topVal = topInput.value;
 let rightVal = rightInput.value;
@@ -13,36 +14,27 @@ changeBorder()
 
 topInput.addEventListener('input', e => {
     topVal = e.target.value;
-    console.log('top val ' + topVal)
     changeBorder()
 })
 
 rightInput.addEventListener('input', e => {
     rightVal = e.target.value;
-    console.log('right val ' + rightVal)
     changeBorder()
 })
 
 bottomInput.addEventListener('input', e => {
     bottomVal = e.target.value;
-    console.log('bottom val ' + bottomVal)
     changeBorder()
 })
 
 leftInput.addEventListener('input', e => {
     leftVal = e.target.value;
-    console.log('left val ' + leftVal)
     changeBorder()
 })
 
 function changeBorder() {
-    borderChanger.style.borderRadius = `${topVal}% ${100-topVal}%
-    ${100-bottomVal}% ${bottomVal}% /
-    ${leftVal}% ${100-rightVal}%
-    ${rightVal}% ${100-leftVal}%`;
+    let borderRad = `${topVal}% ${100-topVal}% ${100-bottomVal}% ${bottomVal}% / ${leftVal}% ${100-rightVal}% ${rightVal}% ${100-leftVal}%`
 
-    console.log(`${topVal}% ${100-topVal}%
-    ${100-bottomVal}% ${bottomVal}% /
-    ${leftVal}% ${100-rightVal}%
-    ${rightVal}% ${100-leftVal}%`)
+    borderChanger.style.borderRadius = borderRad;
+    propertyShower.innerText = borderRad;
 }
