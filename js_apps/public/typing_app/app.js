fd965bc7c9869c6df641e8c221a8c4cd0b550784
Viacheslav <little.cado1@gmail.com>
2020-09-20 22:29:24 +0300

    Add quote garden API to typing app

--- js_apps/public/typing_app/app.js
+++ js_apps/public/typing_app/app.js
@@ -1,10 +1,20 @@
 const frame = document.querySelector('.frame');
 const line = document.querySelector('.line');
-const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi distinctio dolor doloribus ex exercitationem harum hic id ipsam magni maiores officia, placeat';
+const heading = document.querySelector('.heading');
+let text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi distinctio dolor doloribus ex exercitationem harum hic id ipsam magni maiores officia, placeat';
 let step = 0;
 
 document.addEventListener('keydown', event => {
     let key = event.key;
+
+    //Check if user begin typing
+    if (step === 0){heading.style.opacity = 0;}
+
+    // Check if user ended typing
+    if(step === text.length - 1){
+        heading.style.opacity = 1;
+        heading.innerText = 'Good work!🎈🎉✨'
+    }
 
     if (key === text.charAt(step)){
         // if right key pressed
@@ -36,4 +46,15 @@ function addAndRemoveClass(element, className, timeout){
     }, timeout)
 }
 
-fillLine(text)
+async function getRandomQuote() {
+    const api_url = 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
+    const response = await fetch(api_url);
+    return await response.json();
+}
+
+getRandomQuote().then(r => {
+    text = r.quote.quoteText;
+    fillLine(text)
+});
+
+
