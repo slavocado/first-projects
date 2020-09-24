ce8877b6a71d8e87aea5b74aaac2bd6ec9d9888c
Viacheslav <little.cado1@gmail.com>
2020-09-24 11:07:17 +0300

    [typing_app] Add typing speed and loader

--- js_apps/public/typing_app/app.js
+++ js_apps/public/typing_app/app.js
@@ -3,6 +3,7 @@ const line = document.querySelector('.line');
 const heading = document.querySelector('.heading');
 const stats = document.querySelector('.end-screen');
 const restart = document.querySelector('.restart');
+const loader = document.querySelector('#loader')
 let text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi distinctio dolor doloribus ex exercitationem harum hic id ipsam magni maiores officia, placeat';
 let step = 0;
 
@@ -47,10 +48,16 @@ let stopWatch = {
 let statistics = {
     timeElement: document.querySelector('#time'),
     accuracyElement: document.querySelector('#accuracy'),
+    speedElement: document.querySelector('#speed'),
     typos: 0,
     calculateAccuracy: function (totalLetters) {
         return Math.floor(100 - (this.typos / totalLetters * 100)); // in percents
     },
+    wordsInText: 0,
+    calculateSpeed: function () {
+        let seconds = stopWatch.minutes * 60 + stopWatch.seconds;
+        return Math.floor(this.wordsInText / seconds * 60);
+    }
 }
 
 function addAndRemoveClass(element, className, timeout) {
@@ -69,6 +76,8 @@ async function getRandomQuote() {
 getRandomQuote().then(r => {
     text = r.quote.quoteText;
     lineMehods.fill(text);
+    loader.style.display = 'none';
+    heading.innerText = 'Start typing'
 });
 
 
@@ -99,12 +108,14 @@ document.addEventListener('keydown', event => {
     }
 
     // Check if user ended typing
-    if (step === 5) {
+    if (step === text.length - 1) {
         heading.style.opacity = 1;
         heading.innerText = 'Good work!🎈🎉✨';
         stopWatch.stop();
-        statistics.timeElement.innerText = `${stopWatch.minutes} mins ${stopWatch.seconds} seconds`;
+        statistics.timeElement.innerText = `${stopWatch.minutes} min ${stopWatch.seconds} sec`;
         statistics.accuracyElement.innerText = statistics.calculateAccuracy(text.length) + '%';
+        statistics.wordsInText = text.match(/(\w+)/g).length;
+        statistics.speedElement.innerText = statistics.calculateSpeed()+ ' wpm';
         setTimeout(() => {
             stats.classList.add('end-screen-down');
         }, 1000);
