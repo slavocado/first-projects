99a5109d75b329443d315b28f505e23872b27d27
Viacheslav <little.cado1@gmail.com>
2020-10-12 19:16:06 +0300

    [ikea] Add data get from database

--- ikea/script/index.js
+++ ikea/script/index.js
@@ -5,6 +5,7 @@ import generateCatalog from "./generateCatalog.js";
 import generateFooter from "./generateFooter.js";
 import generateHeader from "./generateHeader.js";
 import generateSubCatalog from "./generateSubCatalog.js";
+import { loadData } from "./loadData.js";
 
 
 generateHeader();
@@ -12,3 +13,4 @@ generateFooter();
 generateCatalog();
 generateSubCatalog();
 catalog();
+loadData();
