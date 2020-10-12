c595ab971e059d524c7fc87e170b0f3b4c162384
Viacheslav <little.cado1@gmail.com>
2020-10-12 13:25:32 +0300

    [ikea] Add close submenu

--- ikea/script/main.js
+++ ikea/script/main.js
@@ -4,16 +4,18 @@ const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const subCatalog = document.querySelector('.subcatalog');
 const subcatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
 
 const openMenu = () => {
     catalog.classList.add('open');
     overlay.classList.add('active');
-}
+};
 
 const closeMenu = () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
-}
+    closeSubMenu();
+};
 
 const openSubMenu = event => {
     event.preventDefault();
@@ -22,12 +24,17 @@ const openSubMenu = event => {
         subcatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
     }
+};
+
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
 }
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 catalog.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Escape') {
