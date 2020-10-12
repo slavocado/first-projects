f43ae7bc41fa4a128c9b7320b000947dfcf43061
Viacheslav <little.cado1@gmail.com>
2020-10-12 13:18:17 +0300

    [ikea] Add open submenu

--- ikea/script/main.js
+++ ikea/script/main.js
@@ -2,7 +2,8 @@ const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
-
+const subCatalog = document.querySelector('.subcatalog');
+const subcatalogHeader = document.querySelector('.subcatalog-header');
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -14,9 +15,19 @@ const closeMenu = () => {
     overlay.classList.remove('active');
 }
 
+const openSubMenu = event => {
+    event.preventDefault();
+    const itemList = event.target.closest('.catalog-list__item');
+    if (itemList){
+        subcatalogHeader.innerHTML = itemList.innerHTML;
+        subCatalog.classList.add('subopen');
+    }
+}
+
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
-overlay.addEventListener('click', closeMenu)
+overlay.addEventListener('click', closeMenu);
+catalog.addEventListener('click', openSubMenu);
 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Escape') {
