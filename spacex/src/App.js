e80af47cdb4bb01f5154cc4bbb189bce03d63aa7
Viacheslav <little.cado1@gmail.com>
2020-10-15 22:46:10 +0300

    [spacex] Add hooks in calendar

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -66,12 +66,10 @@ class App extends React.Component{
                 </Route>
 
                 <Route path='/calendar'>
-                    <Main/>
                     <Calendar/>
                 </Route>
 
                 <Route path='/details'>
-                    <Main/>
                     <Details/>
                 </Route>
 
