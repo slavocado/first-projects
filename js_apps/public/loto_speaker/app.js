e309bda29797af8b1e49d2fb2131c14a4898b64b
Viacheslav <little.cado1@gmail.com>
2020-11-16 12:58:49 +0300

    Add number names and chage form to start game

--- js_apps/public/loto_speaker/app.js
+++ js_apps/public/loto_speaker/app.js
@@ -1,9 +1,32 @@
 const container = document.querySelector('.number');
 const speedInput = document.querySelector('#speed-input');
-const modalTitle = document.querySelector('#modal-title');
+const speedInputLabel = document.querySelector('#speed-input-label');
 
 const fromNumber = 1;
 const toNumber = 90;
+const numberNames = [
+    {number: 1, name: 'кол'},
+    {number: 2, name: 'пара'},
+    {number: 3, name: 'троица'},
+    {number: 10, name: 'часовой'},
+    {number: 11, name: 'барабанные палочки'},
+    {number: 12, name: 'дюжина'},
+    {number: 13, name: 'чертова дюжина'},
+    {number: 18, name: 'в первый раз'},
+    {number: 22, name: 'утята'},
+    {number: 25, name: 'опять 25'},
+    {number: 44, name: 'стульчики'},
+    {number: 50, name: 'полста'},
+    {number: 55, name: 'перчатки'},
+    {number: 66, name: 'валенки'},
+    {number: 69, name: 'туда сюда'},
+    {number: 77, name: 'топорики'},
+    {number: 80, name: 'бабушка'},
+    {number: 88, name: 'калачики'},
+    {number: 89, name: 'сосед дедушки'},
+    {number: 90, name: 'дедушка'}
+]
+
 
 let delay = 2000
 
@@ -16,7 +39,7 @@ let interval;
 
 speedInput.addEventListener('input', (e)=>{
     delay = e.target.value * 1000;
-    modalTitle.innerText = `Скорость перебора цифр: ${e.target.value} cек`
+    speedInputLabel.innerText = `Скорость перебора цифр: ${e.target.value} cек`
 })
 
 // https://ru.wikipedia.org/wiki/%D0%A2%D0%B0%D1%81%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%A4%D0%B8%D1%88%D0%B5%D1%80%D0%B0_%E2%80%94_%D0%99%D0%B5%D1%82%D1%81%D0%B0
@@ -40,8 +63,18 @@ function startGame() {
         if (i < toNumber) {
             container.innerText = arr[i];
 
-            let utterance = new SpeechSynthesisUtterance(arr[i]);
-            utterance.rate = 1;
+            let utterance;
+
+            let numberName = numberNames.find(names => names.number === arr[i])
+            if (numberName){
+                let name = numberName.name;
+                utterance = new SpeechSynthesisUtterance(name);
+                utterance.rate = 1.2;
+            } else {
+                utterance = new SpeechSynthesisUtterance(arr[i]);
+                utterance.rate = 1;
+            }
+
             speechSynthesis.speak(utterance);
             i++
         } else {
